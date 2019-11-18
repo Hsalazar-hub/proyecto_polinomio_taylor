b69f3d5ace18e85a5b0bc6a34fd58b0ea303d111
Hector <hdsalazar20@gmail.com>
2019-11-17 22:46:53 -0400

    Proy-Politaylor

--- Hector_25967387.py
+++ Hector_25967387.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
 Proyecto Polinomio de Taylor.
-
 Cada participante debe completar su módulo y luego solicitar el Pull-Request.
 """
 
@@ -11,7 +10,6 @@ import math
 def derivada(f, h = 0.01):
     """
     Retorna la función derivada de f dado un h.
-
     Parámetros:
     f: función de variable real f(x).
     h: tamaño del paso.
@@ -26,16 +24,33 @@ def derivada(f, h = 0.01):
 def polinomio_taylor(f, x0, n):
     """
     Implementa y retorna el Polinomio de Taylor de grado n centrado en x0.
-
     Parámetros:
     f: función de variable real f(x).
     x0: punto centro del polinomio.
     n: grado del polinomio.
     """
     
-    return polinomio
+    
+    def polit(x):
+        i=0
+        dervX = f 
+        C=0  
+        while(i<n):
+            if C != 0:
+                C += ((dervX(x0))/(math.factorial(i)))*((x-x0)**(i))
+                dervX = derivada(dervX)
+            else:
+                C= dervX(x0)
+            i= i+1
+            return C
 
+    return polit
 
 if __name__ == '__main__':
-    # Pruebe aquí el polinomio de Taylor.
-    pass
+   e= lambda P,Q : math.fabs((P-Q)/P)
+   r= lambda x : math.log(x)
+   pt= polinomio_taylor(r,3,6)
+
+   V = 1.1
+
+   print('valor aproximado', pt(V))
